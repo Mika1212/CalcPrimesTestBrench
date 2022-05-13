bef545ffb9eb481cee35e0b9a57fa94011d94354
Mika1212 <ilravgaz03@mail.ru>
2022-05-13 18:04:58 +0300

    Changes

--- src/main/java/org/example/MyBenchmark.java
+++ src/main/java/org/example/MyBenchmark.java
@@ -26,52 +26,49 @@
 package org.example;
 
 import org.openjdk.jmh.annotations.*;
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MyBenchmark {
 
     //Сложность O(N)
-    //Память O(N + M)
+    //Память O(N*2)
     // N - общее количество чисел в заданном диапазоне,
-    // M - количество простых чисел в заданном диапазоне.
     static public int calcPrimesNumberOfEuler(int limit) {
         if (limit < 1) return 0;
         int limit1 = limit + 1;
-        List<Integer> pr = new ArrayList<>();
+        int[] pr = new int[limit1];
+        int currentIndex = 0;
         int[] lp = new int[limit1];
 
         for (int i = 2; i < limit1; i++) {
             if (lp[i] == 0) {
                 lp[i] = i;
-                pr.add(i);
+                pr[currentIndex] = i;
+                currentIndex++;
             }
-            for (Integer p : pr) {
-                if (p <= lp[i] && p * i <= limit) {
+            for (int p: pr){
+                if (p <= lp[i] && p * i <= limit && p != 0) {
                     lp[p * i] = p;
                 } else {
                     break;
                 }
             }
         }
-        pr.add(1);
-        return pr.size();
+        pr[currentIndex] = 1;
+        currentIndex++;
+        return currentIndex;
     }
 
     //Сложность O(N*log(log(N)))
     //Память O(N)
-    static public int calcPrimesNumberOfEratosthenesMassive(int limit) {
+    static public int calcPrimesNumberOfEratosthenes(int limit) {
+        if (limit < 1) return 0;
         int limit1 = limit + 1;
-        if (limit1 <= 1) return 0;
         int answers = 0;
         boolean[] massive = new boolean[limit1];
         Arrays.fill(massive, true);
         massive[0] = false;
-        massive[1] = false;
 
         for (int i = 2; Math.pow(i, 2.0) < limit1; i++) {
             if (massive[i]) {
@@ -88,97 +85,34 @@ public class MyBenchmark {
         return answers;
     }
 
-    //Сложность O(N*log(log(N)))
-    //Память O(N)
-    static public int calcPrimesNumberOfEratosthenes(int limit) {
-        int limit1 = limit + 1;
-        if (limit1 <= 1) return 0;
-        int answers = 0;
-        List<Boolean> list = new ArrayList<>();
-        list.add(false);
-        for (int i = 0; i < limit; i++) {
-            list.add(true);
-        }
-
-        for (int i = 2; Math.pow(i, 2.0) < limit1; i++) {
-            if (list.get(i)) {
-                for (int j = (int) Math.pow(i, 2.0); j < limit1; j += i) {
-                    list.set(j, false);
-                }
-            }
-        }
-
-        for (boolean value: list) {
-            if (value) answers++;
-        }
-
-        return answers;
-    }
-
     @Benchmark @Fork(1) @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.MICROSECONDS)
     public void testMethod() {
-        int n = 100000000;
-
-        //int euler = calcPrimesNumberOfEuler(n);
-        //int eratosthenes = calcPrimesNumberOfEratosthenes(n);
-        int eratosthenes2 = calcPrimesNumberOfEratosthenesMassive(n);
-
-        //int anotherEuler = anotherCalcPrimesNumberOfEuler(n);
-        //int anotherEratosthenes = anotherCalcPrimesNumberOfEratosthenes(n);
+        int n = 100000;
+        int euler = calcPrimesNumberOfEuler(n);
     }
 
-    //Сложность O(N*log(log(N)))
-    //Память O(N)
-    static public int anotherCalcPrimesNumberOfEratosthenes(int limit) {
-        int limit1 = limit + 1;
-        if (limit1 <= 1) return 0;
-        int answers = 0;
-        LinkedList<Boolean> list = new LinkedList<>();
-        list.add(false);
-        for (int i = 0; i < limit; i++) {
-            list.add(true);
-        }
-
-        for (int i = 2; Math.pow(i, 2.0) < limit1; i++) {
-            if (list.get(i)) {
-                for (int j = (int) Math.pow(i, 2.0); j < limit1; j += i) {
-                    list.set(j, false);
-                }
-            }
-        }
-
-        for (boolean value: list) {
-            if (value) answers++;
-        }
-
-        return answers;
+    public static int testEuler(int n) {
+        return calcPrimesNumberOfEuler(n);
     }
 
-    //Сложность O(N)
-    //Память O(N + M)
-    static public int anotherCalcPrimesNumberOfEuler(int limit) {
-        if (limit < 1) return 0;
-        int limit1 = limit + 1;
-        LinkedList<Integer> pr = new LinkedList<>();
-        int[] lp = new int[limit1];
-
-        for (int i = 2; i < limit1; i++) {
-            if (lp[i] == 0) {
-                lp[i] = i;
-                pr.add(i);
-            }
-            for (Integer p : pr) {
-                if (p <= lp[i] && p * i <= limit) {
-                    lp[p * i] = p;
-                } else {
-                    break;
-                }
-            }
-        }
-        pr.add(1);
-        //System.out.println("limit = " + limit + " pr = " + pr);
-        return pr.size();
+    public static int testEratosthenes(int n) {
+        return calcPrimesNumberOfEratosthenes(n);
     }
 
+    public static boolean[] testBool(int n) {
+        boolean[] a = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = true;
+        }
+        return a;
+    }
+
+    public static int[] testInt(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = 1;
+        }
+        return a;
+    }
 }
